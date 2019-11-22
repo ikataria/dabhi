d6dcb67a62d292f00a476b82bd0fdc05bbe0dda5
ikataria <17kataria@gmail.com>
2019-11-22 23:38:01 +0530

    forgotPassword API completed successfully

--- routes/registration/forgotPassword.js
+++ routes/registration/forgotPassword.js
@@ -3,6 +3,7 @@ const ejs = require('ejs')
 const mailer = require('../common/nodemailer')
 const email = require('../common/email_templates/emailData')
 module.exports = (req,res) => {
+    console.log('-------req.body===============>',req.body)
     if(!req.body.email || req.body.email ==  null){
         res.json({
             success :false,
@@ -24,15 +25,29 @@ module.exports = (req,res) => {
             }else{
                 console.log('******regis data*********************************>',regisData)
                 let emailObj = email.forgotPassword(req.decoded.userName, regisData.password )
-                ejs.renderFile(__filename, emailObj, function(err, str){
+                ejs.renderFile(__dirname + '/../common/email_templates/basic.ejs', emailObj, function(err, str){
+                    console.log('=-=-=-=-render file=-=-=-',str)
                     if(err){
+                        console.log('-------here----error---',err)
                         res.json({
                             success : false,
                             msg : 'Something went wrong'
                         })
                     }else{ 
                         let subject = `Your Password for Dabhi social network`
-                        mailer.sendMail(req.body.email,subject,str)
+                        mailer.sendMail(req.body.email,subject,str).then(mailerData => {
+                            console.log('++++++mailer data ++++++++++++',mailerData)
+                            res.json({
+                                success : true,
+                                msg : `Your password for Dabhi is ${mailerData}`
+                            })
+                        }).catch(err => {
+                            res.json({
+                                success: false,
+                                error : err,
+                                msg: "catch err,Go fuck yourself ."
+                            })
+                        })
                     }
                 });
             }
