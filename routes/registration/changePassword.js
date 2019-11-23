57d28bb57d5f4497c34951ce0e970005f57242d4
laptop-00 <Nehashetty@nehashettyn@gmail.com>
2019-11-23 13:24:24 +0530

    changedPassword API complete

--- routes/registration/changePassword.js
+++ routes/registration/changePassword.js
@@ -4,20 +4,22 @@ const dbUserLogin = require('../../model/registration/userLogin')
 
 module.exports = (req, res) => {
     // console.log('>>>>>>>>>>>>>>> Decoded data >>>>>>>>>>>>>>', req.decoded)
-    if (!req.body.password) {
+    if (!req.body.password || req.body.password == null || !req.body.newPassword || req.body.newPassword == null) {
         res.json({
             success: false,
             msg: 'Please fill the password field'
         })
     } else {
-        // first enter old password
-        dbUserLogin.findOne({
-                    $or: [
-                        { userName: req.decoded.userName },
-                        { email: req.decoded.email },
-                        { phone: req.decoded.phone }
-                    ]
-                },
+        //  Mongoose Query 
+        let query = {
+            $or: [
+                { userName: req.decoded.userName },
+                { email: req.decoded.email },
+                { phone: req.decoded.phone }
+            ]
+        }
+
+        dbUserLogin.findOne(query,
                 (err, loginData) => {
                     if (err) {
                         res.json({
@@ -25,14 +27,44 @@ module.exports = (req, res) => {
                             msg: 'Something went wrong'
                         })
                     } else {
-                        if (req.body.password == loginData.password) {
+                        if (req.body.password != loginData.password) {
+                            res.json({
+                                success : false,
+                                msg : 'Incorrect Password'
+                            })
+                        } else if(req.body.password === req.body.newPassword){
+                            res.json({
+                                success : false,
+                                msg : `New & Old Password can't be same`
+                            })
+                        }else{
+                            // Update 
+                            let update = { password : req.body.newPassword}
 
-                        } else {
-                            res.json
+                            dbRegistration.findOneAndUpdate(query,update , (err, rData) => {
+                                if (err) {
+                                    res.json({
+                                        success: false,
+                                        msg: 'Something went wrong'
+                                    })
+                                } else{
+                                    dbUserLogin.findOneAndUpdate(query,{ $set : {password : req.body.newPassword, changePassword : true}} , (err, uData) => {
+                                        if (err) {
+                                            res.json({
+                                                success: false,
+                                                msg: 'Something went wrong'
+                                            })
+                                        } else{
+                                            res.json({
+                                                success : true,
+                                                msg : 'Password updated successfully'
+                                            })
+                                        }
+                                    })
+                                }
+                            })
                         }
                     }
-                })
-            // if(req.body.password == )
-            // if password matches old password, allow user to chnage password else return error 
+                }) 
     }
 }
