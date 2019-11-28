df2b4cca466ee874597e654a06079470837f320e
mohit kataria <mohit@shippigo.com>
2019-11-28 18:09:25 +0530

    showFrnds API bug fixed

--- routes/user/connect/showFrnds.js
+++ routes/user/connect/showFrnds.js
@@ -16,18 +16,38 @@ module.exports = (req, res) => {
                         msg: 'No profile found !'
                     })
                 } else {
-                    dbApproveFR.find({ $and: [{ 'to.DID': profileData.DID }, { approveRequest: 'yes' }] }, (err, approveData) => {
+                    /** use $in for DID  */
+                    const query = { $and: [{ approveRequest: 'yes' }, { $or: [{ 'to.DID': profileData.DID }, { 'from.DID': profileData.DID }] }] }
+                    dbApproveFR.find(query, (err, approveData) => {
+                        console.log('eror=-=-=-=-=-=-=-=-=-=-=-=-=-=^^^^^^^^^^^^^^^^^^^^^^^>', err)
                         if (err) {
                             res.json({
                                 success: false,
                                 msg: 'Something went wrong'
                             })
                         } else {
-                            const frnds = approveData.map(userName => userName.from.userName)
-                            console.log('approve data =-=-=-=-=-=-=-=-=-=-=-=-=-=-=->', frnds)
+                            console.log('approval data=-=-=-=-=-=-=-=-=-=-=-=-=-=^^^^^^^^^^^^^^^^^^^^^^^>', approveData)
+                            const frnds = approveData.map(userName => {
+                                return {
+                                    from: userName.from.userName,
+                                    to: userName.to.userName
+                                }
+                            })
+                            const a = frnds.map(ele => ele.from)
+                            const b = frnds.map(abc => abc.to)
+                            const connected = a.concat(b)
+                                // console.log('%$%$%$%$%$%$%$%$%$%$%$>>', index)
+
+                            const unique = [...new Set(connected)]
+                            const index = unique.indexOf(req.decoded.userName)
+                            if (index != -1) {
+                                unique.splice(index, 1)
+                            }
+                            console.log('decoded data =-=-=-=-=-=-=-=-=-=-=-=-=-=-=->', unique)
+                                // console.log('approve data =-=-=-=-=-=-=-=-=-=-=-=-=-=-=->', unique)
                             res.json({
                                 success: true,
-                                frnds: frnds
+                                frnds: unique
                             })
                         }
                     })
